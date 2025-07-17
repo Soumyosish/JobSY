5444228526bee9804d1e6177c813204ebd042ae5
SoumyosishPal <soumyosishpal.108@gmail.com>
2025-07-17 17:10:45 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -28,10 +28,10 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error(err));
 
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/jobs', require('./routes/jobs'));
-app.use('/api/resume', require('./routes/resume')); // if you separate resume upload
-app.use('/api/goals', require('./routes/goals'));
+app.use('/api/auth', require('./routes/Auth.js'));
+app.use('/api/jobs', require('./routes/Jobs.js'));
+app.use('/api/resume', require('./routes/resume.js')); // if you separate resume upload
+app.use('/api/goals', require('./routes/goals.js'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
